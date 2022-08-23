e832953777a95b60fd04598c65e7e1bb2dc84909
nor0x <nor0x@users.noreply.github.com>
2022-08-23 08:38:53 +0200

    demo cleanup

--- Maui.ColorPicker.Demo/MainPage.xaml.cs
+++ Maui.ColorPicker.Demo/MainPage.xaml.cs
@@ -19,14 +19,7 @@ public partial class MainPage : ContentPage
     private void ColorPicker_PickedColorChanged(object sender, Color colorPicked)
     {
         // Use the selected color
-        SelectedColorDisplayFrame.BackgroundColor = colorPicked;
         SelectedColorValueLabel.Text = colorPicked.ToHex();
-        SelectedColorValueLabel.BackgroundColor = colorPicked;
-        ColorPickerHolderFrame.BackgroundColor = colorPicked;
-
-        if (colorPicked.GetLuminosity() < 0.5)
-            SelectedColorValueLabel.TextColor = Colors.White;
-        else
-            SelectedColorValueLabel.TextColor = Colors.SlateGray;
+        SelectedColorValueLabel.Background = colorPicked;
     }
 }
