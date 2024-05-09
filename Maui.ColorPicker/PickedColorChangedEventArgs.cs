c5f33725374ada14e9ea451b3049708a62af88c6
Johnny <nor0x@users.noreply.github.com>
2024-05-09 09:51:30 +0200

    PickedColorChangedEventArgs added

---
+++ Maui.ColorPicker/PickedColorChangedEventArgs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.ColorPicker;
+
+/// <summary>
+/// Event arguments for PickedColorChanged events. Provides old and new picked color values.
+/// </summary>
+public class PickedColorChangedEventArgs : EventArgs
+{
+	/// <summary>
+	/// Creates a new <see cref="PickedColorChangedEventArgs"/> with <paramref name="oldColorValue"/>
+	/// and <paramref name="newColorValue"/>
+	/// </summary>
+	/// <param name="oldColorValue">The old picked color value.</param>
+	/// <param name="newColorValue">The new picked color value.</param>
+	public PickedColorChangedEventArgs(Color? oldColorValue, Color newColorValue)
+	{
+		OldPickedColorValue = oldColorValue;
+		NewPickedColorValue = newColorValue;
+	}
+
+	/// <summary>
+	/// Gets the new picked color value.
+	/// </summary>
+	public Color NewPickedColorValue { get; init; }
+
+	/// <summary>
+	/// Gets the old picked color value.
+	/// </summary>
+	public Color? OldPickedColorValue { get; init; }
+}
