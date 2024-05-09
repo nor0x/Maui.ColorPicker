1924668eb6bf26235df1f62bc8688bf0ea98c9e7
Johnny <nor0x@users.noreply.github.com>
2024-05-09 09:51:16 +0200

    fixes for #1 and #4
    some fixes borrowed from https://github.com/trungnt2910/ColorPicker.Maui

--- Maui.ColorPicker.Demo/MainPage.xaml.cs
+++ Maui.ColorPicker.Demo/MainPage.xaml.cs
@@ -2,14 +2,45 @@
 
 public partial class MainPage : ContentPage
 {
-    public MainPage()
-    {
-        InitializeComponent();
-    }
-    private void ColorPicker_PickedColorChanged(object sender, Color colorPicked)
-    {
-        // Use the selected color
-        SelectedColorValueLabel.Text = colorPicked.ToHex();
-        SelectedColorValueLabel.Background = colorPicked;
-    }
+	private bool _valueChanging = false;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		_valueChanging = true;
+		SelectedColorValueEntry.Text = ColorPicker.PickedColor.ToHex();
+		SelectedColorValueEntry.TextColor = ColorPicker.PickedColor.GetComplementary();
+		SelectedColorValueEntry.Background = ColorPicker.PickedColor;
+		_valueChanging = false;
+	}
+
+	private void ColorPicker_PickedColorChanged(object sender, PickedColorChangedEventArgs e)
+	{
+		// This check is requried as the function may be called really early in the
+		// app's lifetime.
+		if (SelectedColorValueEntry != null && !_valueChanging)
+		{
+			_valueChanging = true;
+			// Use the selected color
+			SelectedColorValueEntry.Text = e.NewPickedColorValue.ToHex();
+			SelectedColorValueEntry.TextColor = e.NewPickedColorValue.GetComplementary();
+			SelectedColorValueEntry.Background = e.NewPickedColorValue;
+			_valueChanging = false;
+		}
+	}
+
+	private void SelectedColorValueEntry_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		if (ColorPicker != null && !_valueChanging)
+		{
+			if (Color.TryParse(SelectedColorValueEntry.Text, out var color))
+			{
+				_valueChanging = true;
+				ColorPicker.PickedColor = color;
+				SelectedColorValueEntry.TextColor = color.GetComplementary();
+				SelectedColorValueEntry.Background = color;
+				_valueChanging = false;
+			}
+		}
+	}
 }
